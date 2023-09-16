fe6b83cd9992730abb6f10b49acca272003ff754
ChiaraManeo <111811938+ChiaraManeo@users.noreply.github.com>
2023-09-16 18:56:50 -0300

    ChuckNorris.py

--- Consumo_API/api2.py
+++ Consumo_API/api2.py
@@ -1,13 +1,13 @@
 import requests
 
-url = "https://api.kanye.rest/"
+url = "https://api.chucknorris.io/jokes/random"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    
     data = response.json()
-    frase = data['quote']
-    print("Frase de Kanye West:", frase)
+    frase = data['value']
+    print('Fatos sobre Chuck:', frase)
+
 else:
     print("Erro na solicitação:", response.status_code)
